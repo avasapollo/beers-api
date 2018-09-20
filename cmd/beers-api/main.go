92fa4e7ea8aeded783d11dbd24f8b80190a36c4c
Andrea Vasapollo <a.vasapollo@faceit.com>
2018-09-20 15:25:02 +0100

    fix(cmd): add eventhub

--- cmd/beers-api/main.go
+++ cmd/beers-api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/avasapollo/beers-api/beers"
 	"github.com/avasapollo/beers-api/database"
+	"github.com/avasapollo/beers-api/eventhub"
 	"github.com/avasapollo/beers-api/reviews"
 	"github.com/avasapollo/beers-api/web"
 	"github.com/sirupsen/logrus"
@@ -14,10 +15,11 @@ func main() {
 
 	// database
 	db := database.NewMemoryDB(le, database.GetInitBeers(), database.GetInitReviews())
+	brokerSvc := eventhub.NewService(le)
 
 	// services
-	beersSvc := beers.NewService(le, db)
-	reviewsSvc := reviews.NewService(le, db)
+	beersSvc := beers.NewService(le, db, brokerSvc)
+	reviewsSvc := reviews.NewService(le, db, brokerSvc)
 
 	web.NewRestApi(le, beersSvc, reviewsSvc).ListenServe()
 }
